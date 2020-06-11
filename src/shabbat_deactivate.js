ce5c3051360248c1b549c6c43fd7d334a01a9427
Michael J. Radwin <michael@radwin.org>
2020-06-11 16:10:31 -0700

    Use mysql promises for compactness

--- src/shabbat_deactivate.js
+++ src/shabbat_deactivate.js
@@ -1,6 +1,7 @@
 /* eslint-disable require-jsdoc */
 import fs from 'fs';
 import ini from 'ini';
+import util from 'util';
 import mysql from 'mysql';
 import pino from 'pino';
 import minimist from 'minimist';
@@ -20,6 +21,7 @@ argv.count = argv.count || COUNT_DEFAULT;
 const logger = pino({prettyPrint: {translateTime: true, ignore: 'pid,hostname'}});
 const iniPath = argv.ini || '/home/hebcal/local/bin/hebcal-dot-com.ini';
 const config = ini.parse(fs.readFileSync(iniPath, 'utf-8'));
+let logdir = '/home/hebcal/local/var/log';
 
 main(argv.sleeptime)
     .then(() => {
@@ -30,59 +32,76 @@ main(argv.sleeptime)
       process.exit(1);
     });
 
-async function main() {
-  const connection = mysql.createConnection({
-    host: config['hebcal.mysql.host'],
-    user: config['hebcal.mysql.user'],
-    password: config['hebcal.mysql.password'],
-    database: config['hebcal.mysql.dbname'],
-  });
+function makeDb(config) {
+  const connection = mysql.createConnection(config);
   connection.connect(function(err) {
     if (err) {
       logger.fatal(err);
       throw err;
     }
-    logger.debug('connected as id ' + connection.threadId);
   });
-
-  const addrs = await getCandidates(connection);
-  logger.info(`Deactivating ${addrs.length} subscriptions`);
-  if (!argv.dryrun && addrs.length) {
-    await deactivateSubs(connection, addrs);
-  }
-  connection.end();
+  const connQuery = util.promisify(connection.query);
+  const connEnd = util.promisify(connection.end);
+  return {
+    query(sql, args) {
+      return connQuery.call(connection, sql, args);
+    },
+    close() {
+      return connEnd.call(connection);
+    },
+  };
 }
 
-async function deactivateSubs(connection, addrs) {
+async function getLogDir() {
+  try {
+    const stats = await fstat(logdir);
+    if (!stats || !stats.isDirectory()) {
+      logdir = '.';
+    }
+  } catch (err) {
+    logdir = '.';
+  }
+}
+
+async function main() {
+  const db = makeDb({
+    host: config['hebcal.mysql.host'],
+    user: config['hebcal.mysql.user'],
+    password: config['hebcal.mysql.password'],
+    database: config['hebcal.mysql.dbname'],
+  });
+  await getLogDir();
+  const addrs = await getCandidates(db);
+  logger.info(`Deactivating ${addrs.length} subscriptions`);
+  if (!argv.dryrun && addrs.length) {
+    await deactivateSubs(db, addrs);
+  }
+  return db.close();
+}
+
+async function deactivateSubs(db, addrs) {
   const emails = addrs.join('\',\'');
   const sql1 = `UPDATE hebcal_shabbat_email
   SET email_status='bounce' WHERE email_address IN('${emails}')`;
-  const p1 = new Promise((resolve, reject) => {
-    connection.query(sql1, function(error, results) {
-      if (error) return reject(error);
-      return resolve(addrs);
-    });
-  });
+  await db.query(sql1);
   const sql2 = `UPDATE hebcal_shabbat_bounce
   SET deactivated=1 WHERE email_address IN('${emails}')`;
-  const p2 = new Promise((resolve, reject) => {
-    connection.query(sql2, function(error, results) {
-      if (error) return reject(error);
-      return resolve(addrs);
-    });
-  });
-  const p3 = new Promise((resolve, reject) => {
-    const subsPath = '/home/hebcal/local/var/log/subscribers.log';
+  await db.query(sql2);
+  return new Promise((resolve, reject) => {
+    const subsPath = logdir + '/subscribers.log';
     const t = Math.floor(new Date().getTime() / 1000);
-    const logStream = fs.createWriteStream(subsPath, {flags: 'a'});
-    addrs.forEach((addr) => logStream.write(`status=1 to=${addr} code=bounce time=${t}\n`));
-    logStream.end();
-    return resolve(true);
+    try {
+      const logStream = fs.createWriteStream(subsPath, {flags: 'a'});
+      addrs.forEach((addr) => logStream.write(`status=1 to=${addr} code=deactivated time=${t}\n`));
+      logStream.end();
+      return resolve(true);
+    } catch (err) {
+      return reject(err);
+    }
   });
-  return Promise.all([p1, p2, p3]);
 }
 
-async function getCandidates(connection) {
+async function getCandidates(db) {
   const reasons = argv.reasons.split(',');
   const reasonsSql = reasons.join('\',\'');
   const sql = `
@@ -95,21 +114,17 @@ AND b.std_reason IN('${reasonsSql}')
 AND b.deactivated = 0
 GROUP by b.email_address,std_reason`;
   logger.info(sql);
-  return new Promise((resolve, reject) => {
-    connection.query(sql, function(error, results) {
-      if (error) return reject(error);
-      const addrs = [];
-      for (const row of results) {
-        if (row.count > argv.count || row.std_reason == 'amzn_abuse') {
-          if (!argv.quiet) {
-            logger.info(`${row.email_address} (${row.count} bounces)`);
-          }
-          addrs.push(row.email_address);
-        }
+  const results = await db.query(sql);
+  const addrs = [];
+  for (const row of results) {
+    if (row.count > argv.count || row.std_reason == 'amzn_abuse') {
+      if (!argv.quiet) {
+        logger.info(`${row.email_address} (${row.count} bounces)`);
       }
-      return resolve(addrs);
-    });
-  });
+      addrs.push(row.email_address);
+    }
+  }
+  return Promise.resolve(addrs);
 }
 
 function usage() {
