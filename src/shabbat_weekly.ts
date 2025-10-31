85b8607b4721198036a220d2a391961189c93121
Michael J. Radwin <michael@radwin.org>
2025-10-31 07:11:18 -0700

    Add molad to weekly shabbat email

--- src/shabbat_weekly.ts
+++ src/shabbat_weekly.ts
@@ -343,6 +343,7 @@ function getSubjectAndBody(cfg: CandleConfig): string[] {
     il: location.getIsrael(),
     sedrot: true,
     shabbatMevarchim: true,
+    molad: true,
     useElevation: cfg.ue,
   };
   if (typeof cfg.m === 'number') {
