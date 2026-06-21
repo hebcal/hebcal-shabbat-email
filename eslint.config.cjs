8f0e75762fc7592f0b08d8d9be29b6ca99b9bac5
Michael J. Radwin <michael@radwin.org>
2026-06-21 09:08:53 -0700

    build: cleanup eslint config

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,31 +1,10 @@
-let customConfig = [
+module.exports = [
   {
     ignores: ['dist/**', 'shabbat_weekly.rewrite.ts'],
   },
-];
-let hasIgnoresFile = false;
-try {
-  require.resolve('./eslint.ignores.js');
-  hasIgnoresFile = true;
-} catch {
-  // eslint.ignores.js doesn't exist
-}
-
-if (hasIgnoresFile) {
-  const ignores = require('./eslint.ignores.js');
-  customConfig.push({ignores});
-}
-
-const tseslint = require('typescript-eslint');
-
-module.exports = [
-  ...customConfig,
   ...require('gts'),
   {
     files: ['**/*.ts', '**/*.tsx'],
-    plugins: {
-      '@typescript-eslint': tseslint.plugin,
-    },
     rules: {
       '@typescript-eslint/no-explicit-any': 'warn',
     },
